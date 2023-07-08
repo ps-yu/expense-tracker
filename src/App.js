2a158fb3d0f777a7f63b08f984ad920da6524685
ps-yu <pankajsharma12540@gmail.com>
2023-07-08 14:23:28 -0400

    render components in the app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import React from 'react';
 import './App.css';
+import { Header } from './components/Header';
 
 function App() {
   return (
-    <>
+    <div>
+      <Header />
       Hello world!!
-    </>
+    </div>
   );
 }
 
