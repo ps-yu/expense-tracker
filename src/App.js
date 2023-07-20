c6bf55d8f43c5bdafbfe869cce8a5c908287695d
ps-yu <pankajsharma12540@gmail.com>
2023-07-19 22:59:29 -0400

    Adding form to add new expenses

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Expenses from "./components/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 function App() {
   //Dummy data to be used as a prop for the expeneseItem components
   const expenses = [
@@ -25,8 +26,8 @@ function App() {
 
   return (
     <div>
-      <h2>Let's get started!</h2>
       <div className="expenses">
+        <NewExpense />
         <Expenses expenses={expenses}/>
       </div>
     </div>
---
+++ src/components/NewExpense/ExpenseForm.js
@@ -0,0 +1,26 @@
+import './ExpenseForm.css'
+
+function ExpenseForm (){
+  return(
+    <form>
+      <div className='new-expense__controls'>
+        <div className='new-expense__control'>
+          <label>Title</label>
+          <input type='text' />
+        </div>
+        <div className='new-expense__control'>
+          <label>Amount</label>
+          <input type='number' />
+        </div>
+        <div className='new-expense__control'>
+          <label>date</label>
+          <input type='date' />
+        </div>
+      </div>
+      <div className='new-expense__actions'>
+        <button type='submit'>Add Expense</button>
+      </div>
+    </form>
+  )
+}
+export default ExpenseForm;
