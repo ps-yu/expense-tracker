3c15247c59e6493c05874514735ad11a4305000f
ps-yu <pankajsharma12540@gmail.com>
2023-07-19 22:28:49 -0400

    Add expense tracker component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,14 +1,20 @@
+import React, {useState} from "react";
 import "./ExpenseItem.css"
 import ExpenseDate from "./expenseDate";
 
 
 function ExpenseItem(props) {
+  const [title, setTitle] = useState(props.expenses.title);
+  const clickHandler = () => {
+    setTitle('Update')
+  }
   return (
     <div className="expense-item">
       < ExpenseDate date = {props.expenses.date} />
       <div className="expense-item__description">
-        <h2> {props.expenses.title} </h2>
+        <h2> {title} </h2>
         <div className="expense-item__price"> {props.expenses.amount} </div>
+        <button onClick ={clickHandler}> Update</button>
       </div>
     </div>
   );
